1b631df2365e48b02c268fb5a8f992d57655cc30
Jon Loucks <j.loucks@comcast.net>
2025-11-10 19:42:12 -0500

    uses

--- concurrency-smoke/src/main/java/module-info.java
+++ concurrency-smoke/src/main/java/module-info.java
@@ -4,8 +4,8 @@
 module io.github.jonloucks.concurrency.smoke {
     requires transitive io.github.jonloucks.concurrency;
     
-//    uses io.github.jonloucks.contracts.api.ContractsFactory;
-//    uses io.github.jonloucks.concurrency.api.ConcurrencyFactory;
+    uses io.github.jonloucks.contracts.api.ContractsFactory;
+    uses io.github.jonloucks.concurrency.api.ConcurrencyFactory;
     
     exports io.github.jonloucks.concurrency.smoke;
 }
